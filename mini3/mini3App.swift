f3ffe5a44efabceb4e2e60c8608fe8fbffa7cd10
Henrique Alves <henriqueasemmer@gmail.com>
2023-10-28 13:51:52 -0300

    Initial Commit

---
+++ mini3/mini3App.swift
@@ -0,0 +1,17 @@
+//
+//  mini3App.swift
+//  mini3
+//
+//  Created by Henrique Semmer on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct mini3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
