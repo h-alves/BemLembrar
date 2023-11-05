1eb614b0adc1e9bd97b8888e757e8203f6bceb63
Henrique Alves <henriqueasemmer@gmail.com>
2023-11-05 18:11:32 -0300

    Botão pra navegar

--- mini3/View/Preferences/SmellView.swift
+++ mini3/View/Preferences/SmellView.swift
@@ -9,32 +9,53 @@ import SwiftUI
 
 struct SmellView: View {
     var body: some View {
-        VStack(spacing: 0) {
-            TitleBar(symbol: "nose", title: "Cheiros")
-            
-            ScrollView {
-                VStack {
-                    HStack {
-                        Text("Quais gostam de cheiro...")
-                            .font(.system(size: 24))
-                            .fontWeight(.semibold)
+        ZStack {
+            VStack(spacing: 0) {
+                TitleBar(symbol: "nose", title: "Cheiros")
+                
+                ScrollView {
+                    VStack {
+                        HStack {
+                            Text("Quais gostam de cheiro...")
+                                .font(.system(size: 24))
+                                .fontWeight(.semibold)
+                            
+                            Spacer()
+                        }
+                        .frame(maxWidth: .infinity)
                         
-                        Spacer()
-                    }
-                    .frame(maxWidth: .infinity)
-                    
-                    VStack(spacing: 24) {
-                        ForEach(Cheiro.allCases, id: \.rawValue) { c in
-                            PreferenceCard(cheiro: c,clientList: .constant([Client.test])) {
-                                print("a")
+                        VStack(spacing: 24) {
+                            ForEach(Cheiro.allCases, id: \.rawValue) { c in
+                                PreferenceCard(cheiro: c,clientList: .constant([Client.test])) {
+                                    print("a")
+                                }
                             }
                         }
                     }
+                    .padding(.top, 24)
+                    .padding(.horizontal, 36)
                 }
-                .padding(.top, 24)
-                .padding(.horizontal, 36)
+                .scrollIndicators(.hidden)
             }
-            .scrollIndicators(.hidden)
+            
+            VStack {
+                Spacer()
+                
+                Button {
+                    RouterService.shared.navigate(.skin)
+                } label: {
+                    Text("Próximo ->")
+                        .fontWeight(.bold)
+                        .foregroundStyle(.black)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color(.systemGray4))
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
+                .padding(.top, 12)
+                .padding(.horizontal, 36)
+                .background(.gray)
+            }
         }
     }
 }
