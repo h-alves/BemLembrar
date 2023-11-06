31ce6f1bc30ca6d1379408be8acdf691f5be4938
Gabriel <gabriel.meira@outlook.com>
2023-11-06 15:07:38 -0300

    Client View de forma estatica

--- mini3/View/ClientView.swift
+++ mini3/View/ClientView.swift
@@ -9,11 +9,105 @@ import SwiftUI
 
 struct ClientView: View {
     var body: some View {
-        Button {
-            print("a")
-        } label: {
-            Text("Client")
+        VStack {
+            Circle()
+                .frame(maxWidth: 200, maxHeight: 200)
+                .foregroundColor(.gray)
+                .padding(.top, 32)
+            
+            Text("Nome da cliente")
+                .font(.system(size: 34))
+            
+            Button(action: {
+                        // Coloque a ação que você deseja executar quando o botão for pressionado aqui
+                        print("Botão pressionado!")
+                    }) {
+                        Text("Entrar em contato")
+                            .font(.system(size: 22))
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    .frame(maxWidth: 298, maxHeight: 46)
+                    .background(Color.black)
+                    .cornerRadius(10)
         }
+        .padding(.bottom, 12)
+        
+        VStack(alignment: .leading) {
+            
+            Text("Preferências:")
+                .font(.system(size: 17))
+                .bold()
+            
+            Text("Lembre-se do que sua cliente mais gosta!")
+                .font(.system(size: 13))
+            
+            HStack {
+                
+                Rectangle()
+                    .frame(maxWidth: 57, maxHeight: 18)
+                    .cornerRadius(8)
+                    .overlay(
+                        Text("base 07")
+                            .foregroundColor(.white)
+                            .font(.system(size: 12))
+                    )
+                
+                Rectangle()
+                    .frame(maxWidth: 57, maxHeight: 18)
+                    .cornerRadius(8)
+                    .overlay(
+                        Text("doce")
+                            .foregroundColor(.white)
+                            .font(.system(size: 12))
+                    )
+                
+                Rectangle()
+                    .frame(maxWidth: 57, maxHeight: 18)
+                    .cornerRadius(8)
+                    .overlay(
+                        Text("ligação")
+                            .foregroundColor(.white)
+                            .font(.system(size: 12))
+                    )
+                
+                Rectangle()
+                    .frame(maxWidth: 57, maxHeight: 18)
+                    .cornerRadius(8)
+                    .overlay(
+                        Text("pele oleosa")
+                            .foregroundColor(.white)
+                            .font(.system(size: 12))
+                    )
+                
+            }
+        }
+        .frame(width: 330, height: 99)
+        .background(Color(.systemGray5))
+        .cornerRadius(12)
+        .padding(0)
+        .padding(.bottom, 12)
+        
+        VStack(alignment: .leading) {
+            
+            Text("Anotações:")
+                .font(.system(size: 17))
+                .bold()
+                .padding(.horizontal, 12)
+            
+            Text("Aqui você pode registrar alergias, tanana e \ndetalhes para garantir sempre o melhor atendimento!")
+                .font(.system(size: 13))
+                .padding(.horizontal, 12)
+            
+            // Verificar o espaçamento dentro do VStack (retangulo)
+            
+        }
+        .frame(width: 330, height: 109)
+        .background(Color(.systemGray5))
+        .cornerRadius(12)
+        
+        Spacer()
+        
     }
 }
 
