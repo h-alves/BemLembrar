ca67683cd6dd1e7943ff52ee4ad978f4663db3eb
Henrique Alves <henriqueasemmer@gmail.com>
2023-11-10 00:34:14 -0300

    Botão de ir para configurações direto

--- bemLembrar/View/RejectedView.swift
+++ bemLembrar/View/RejectedView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct RejectedView: View {
     var body: some View {
-        VStack {
-            Text("LIBERA OS CONTATOS")
+        VStack(spacing: 24) {
+            Text("Precisamos de acesso aos seus contatos para adicionar clientes")
+                .multilineTextAlignment(.center)
+            
+            Button {
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            } label: {
+                Text("Ir para as configurações")
+                    .foregroundStyle(.white)
+                    .fontWeight(.bold)
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
         }
+        .padding(.horizontal, 40)
     }
 }
 
