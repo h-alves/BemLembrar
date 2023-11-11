8d88ad1413e00276491bd95e9dfe8b0368bbbeca
Henrique Alves <henriqueasemmer@gmail.com>
2023-11-11 14:26:53 -0300

    Estrutura do onboarding

--- bemLembrar/View/OnboardingView.swift
+++ bemLembrar/View/OnboardingView.swift
@@ -8,14 +8,71 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    @State var selectIndex = 0
+    
     var body: some View {
         VStack {
+            Spacer()
+            
+            VStack(alignment: .leading, spacing: 6) {
+                Text("Informações das suas clientes sempre bem lembradas!")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 32))
+                    .fontWeight(.semibold)
+                
+                Text("Monte sua lista de clientes e salve tudo sobre elas para facilitar o contato. Simples assim:")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 16))
+            }
+            
+            Spacer()
+            
+            VStack {
+                HStack(alignment: .center, spacing: 12) {
+                    ForEach((0..<3), id: \.self) { index in
+                        if index > self.selectIndex {
+                            Circle()
+                                .stroke(lineWidth: 2)
+                                .foregroundColor(.white)
+                                .frame(width: 16, height: 16)
+                        } else {
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 16, height: 16)
+                        }
+                    }
+                }
+                .animation(.default, value: selectIndex)
+                .frame(maxWidth: .infinity, maxHeight: 20)
+                
+                TabView(selection: $selectIndex) {
+                    ForEach(0..<3, id: \.self) { index in
+                        Image("preferenceImage\(selectIndex+1)")
+                            .resizable()
+                            .frame(width: 241, height: 241)
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                .frame(height: 400)
+            }
+            
+            Spacer()
+            
             Button {
                 RouterService.shared.navigate(.allClients)
             } label: {
-                Text("Onboarding")
+                Text("Continuar")
+                    .foregroundStyle(.black)
+                    .fontWeight(.bold)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background()
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
             }
         }
+        .padding(.horizontal, 36)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(.darkGray))
     }
 }
 
