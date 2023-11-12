d84cd0da0fb356863bfc4c1de1048a1f421c30a7
Henrique Alves <henriqueasemmer@gmail.com>
2023-11-12 13:46:23 -0300

    Edição de marcas e estratégias do usuário

--- bemLembrar/ViewModel/UserViewModel.swift
+++ bemLembrar/ViewModel/UserViewModel.swift
@@ -12,4 +12,39 @@ class UserViewModel: ObservableObject {
     @Published var image = UserData.shared.user.image
     @Published var brands = UserData.shared.user.brands
     @Published var strategies = UserData.shared.user.strategies
+    
+    @Published var brandIsEditing = false
+    @Published var strategyIsEditing = false
+    
+    func selectBrand(brand: Brand) {
+        for (index, originalBrand) in brands.enumerated() {
+            if originalBrand == brand {
+                brands[index].isSelected.toggle()
+            }
+        }
+    }
+    
+    func saveBrands() {
+        for (index, brand) in brands.enumerated() {
+            UserData.shared.user.brands[index] = brand
+        }
+        
+        brandIsEditing.toggle()
+    }
+    
+    func selectStrategy(strategy: Strategy) {
+        for (index, originalStrategy) in strategies.enumerated() {
+            if originalStrategy == strategy {
+                strategies[index].isSelected.toggle()
+            }
+        }
+    }
+    
+    func saveStrategies() {
+        for (index, strategy) in strategies.enumerated() {
+            UserData.shared.user.strategies[index] = strategy
+        }
+        
+        strategyIsEditing.toggle()
+    }
 }
