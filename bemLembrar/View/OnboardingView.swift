f489887b049a10929079b05ac6489021db47d975
Henrique Alves <henriqueasemmer@gmail.com>
2023-11-14 22:03:19 -0300

    Onboarding completo

--- bemLembrar/View/OnboardingView.swift
+++ bemLembrar/View/OnboardingView.swift
@@ -12,74 +12,93 @@ struct OnboardingView: View {
     
     @State var selectIndex = 0
     
+    let descriptionTexts = ["Comece criando sua lista de clientes a partir dos seus contatos.", "Identifique as clientes em sua lista de contatos e registre suas preferências!", "Receba notificações sobre suas clientes para fortalecer seus laços."]
+    
     var body: some View {
         VStack {
             Spacer()
             
-            VStack(alignment: .leading, spacing: 6) {
+            VStack(alignment: .leading, spacing: 10) {
                 Text("Informações das suas clientes sempre bem lembradas!")
-                    .foregroundStyle(.white)
-                    .font(.system(size: 32))
+                    .foregroundStyle(.preto)
+                    .font(.largeTitle)
                     .fontWeight(.semibold)
                 
                 Text("Monte sua lista de clientes e salve tudo sobre elas para facilitar o contato. Simples assim:")
-                    .foregroundStyle(.white)
-                    .font(.system(size: 16))
+                    .foregroundStyle(.preto)
+                    .font(.body)
             }
             
             Spacer()
             
-            VStack {
-                HStack(alignment: .center, spacing: 12) {
+            VStack(spacing: 24) {
+                TabView(selection: $selectIndex) {
+                    ForEach(0..<3, id: \.self) { index in
+                        VStack {
+                            HStack {
+                                Text("\(selectIndex+1)")
+                                    .foregroundStyle(.preto)
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    .padding()
+                                    .background(.rosa)
+                                    .clipShape(Circle())
+                                
+                                Text(descriptionTexts[selectIndex])
+                                    .foregroundStyle(.preto)
+                                    .font(.headline)
+                                    .frame(width: 230)
+                            }
+                            
+                            Image("onboardingImage\(selectIndex+1)")
+                                .resizable()
+                                .frame(width: 225, height: 225)
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                .padding()
+                .frame(maxWidth: 329)
+                .frame(maxHeight: 340)
+                .background(.branco)
+                .clipShape (
+                    RoundedRectangle(cornerRadius: 12)
+                )
+                .shadow(radius: 5)
+                
+                HStack(alignment: .center, spacing: 4) {
                     ForEach((0..<3), id: \.self) { index in
-                        if index > self.selectIndex {
-                            Circle()
-                                .stroke(lineWidth: 2)
-                                .foregroundColor(.white)
-                                .frame(width: 16, height: 16)
+                        if index != self.selectIndex {
+                            Image(systemName: "circle")
+                                .foregroundStyle(.preto)
+                                .font(.system(size: 14))
+                                .fontWeight(.semibold)
                         } else {
-                            Circle()
-                                .fill(.white)
-                                .frame(width: 16, height: 16)
+                            Image(systemName: "circle.fill")
+                                .foregroundStyle(.preto)
+                                .font(.system(size: 14))
+                                .fontWeight(.semibold)
                         }
                     }
                 }
                 .animation(.default, value: selectIndex)
                 .frame(maxWidth: .infinity, maxHeight: 20)
-                
-                TabView(selection: $selectIndex) {
-                    ForEach(0..<3, id: \.self) { index in
-                        Image("preferenceImage\(selectIndex+1)")
-                            .resizable()
-                            .frame(width: 241, height: 241)
-                    }
-                }
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                .frame(height: 400)
             }
             
             Spacer()
             
             if selectIndex >= 2 {
-                Button {
+                BLButton(symbol: "", text: "Começar", disabled: false, opposite: false, color: .branco, textColor: .verde) {
                     onboarding = false
                     RouterService.shared.navigate(.allClients)
-                } label: {
-                    Text("Continuar")
-                        .foregroundStyle(.black)
-                        .fontWeight(.bold)
                 }
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background()
-                .clipShape(RoundedRectangle(cornerRadius: 12))
             } else {
-                Spacer(minLength: 55)
+                Spacer(minLength: 39)
             }
         }
-        .padding(.horizontal, 36)
+        .padding(.horizontal, 32)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(.darkGray))
+        .background(.rosa)
         .onChange(of: selectIndex, perform: { value in
             if selectIndex == 2 {
                 NotificationManager.shared.requestPermission()
